f69278ca81026c99004c136f6b9b84064a963fe4
a.komar <kom07art@gmail.com>
2020-04-11 18:11:32 +0300

    - add missed release package

---
+++ auth/src/release/kotlin/com/duglasher/fitbitauth/utils/OkHttpClientCreator.kt
@@ -0,0 +1,15 @@
+package com.duglasher.fitbitauth.utils
+
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+
+object OkHttpClientCreator {
+
+	fun create() = OkHttpClient.Builder().apply {
+		connectTimeout(30, TimeUnit.SECONDS)
+		readTimeout(30, TimeUnit.SECONDS)
+		writeTimeout(30, TimeUnit.SECONDS)
+	}.build()
+
+}
